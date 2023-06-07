61978f0a3bf2095837aaaf6ee0004f053136fd50
will <wil.fabri777@gmail.com>
2023-06-07 17:50:36 -0400

    master

---
+++ src2/app.ts
@@ -0,0 +1,32 @@
+import express, { application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import UserController from './controllers/usuario.controller';
+import LibroController from './controllers/libro.controller';
+
+const app = express();
+
+app.use(cors({
+    exposedHeaders: ['Authorization']
+}));
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((err:any, req:any, res:any, next:any) => {
+	if (err) {
+	  console.error('Invalid Request data')
+	  res.send('Petición de request invalido')
+	} else {
+	  next();
+	}
+});
+
+app.get('/api/usuario/test',UserController.test);
+app.get('/api/usuario/list',UserController.list);
+app.post('/api/usuario/create',UserController.registrar);
+app.put('/api/usuario/edit/:id',UserController.editar);
+app.delete('/api/usuario/delete/:id',UserController.eliminar);
+
+app.get('/api/libro/list',LibroController.list);
+
+export default app;
